64d2382e460af5d3ed5b52177f27cc4f55d1d0ac
Javis <javischen9548@foxmail.com>
2021-09-28 18:52:05 +0800

    init

---
+++ src/app.config.ts
@@ -0,0 +1,23 @@
+export default {
+  pages: [
+    'pages/index/index'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  },
+  tarBar:{
+    list: [
+      {
+        text:"学习",
+        pagePath: 'pages/index/index'
+      },
+      {
+        text:"我的",
+        pagePath: 'pages/index/index'
+      },
+    ]
+  }
+}
